31561b84dd0dfd134dec4f0b465b7b8d8c9fd26f
Hari Prasath <108864778+Hariprasathstack@users.noreply.github.com>
2022-07-27 13:57:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "Hari Prasath P",
+      "email": "personal.hariprasath@gamil.com",
+      "phone": 7738167119,
+      "degree": "B.E",
+      "location": {
+        "address": "11/86, Salaipudur, Dindigul, Tamilnadu",
+        "postalCode": 624708,
+        "city": "Dindigul",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/hari-prasath-b4121011b/",
+          "github":"https://github.com/Hariprasathstack"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Quest Global",
+        "position": "Senior Engineer Instrumentation",
+        "startDate": "28-Aug-2019",
+        "endDate": "Present",
+        "summary": "I am Instrumentaation Engineer with complete ownership on Industrial projects",
+
+      },
+    ],
+    "education": [
+      {
+        "institution": "Bannari Amman Institute of Technology",
+        "department": "E&I",
+        "studyType": "fulltime",
+        "batch start year": 2010,
+        "batch end year": 2014,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,HTML,CSS",
+        "level": "beginner",
+        "project": [
+          "Learn the code and practice in codekata"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English,Hindi",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Swimming, Hiking",
+      }
+    ]
+  }
+  console.log(myResume);
